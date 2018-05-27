e9eb6ca682c256723bde0aabcb75cd6745b2feac
Ivan Chistyakov <ivanchistyakov@mba.local>
2018-05-27 20:28:11 +0300

    Dock icon and main window are removed, menu bar is added.

--- easyTor/AppDelegate.swift
+++ easyTor/AppDelegate.swift
@@ -11,16 +11,31 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Enable VPN", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: ""))
+        
+        statusItem.menu = menu
+    }
+    
 }
 
