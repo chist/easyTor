9f742ee202bdcfb5cfdeb3b578689f234faf6d77
Ivan Chistyakov <ivanchistyakov@mba.local>
2018-05-27 20:06:28 +0300

    Initial Commit

---
+++ easyTor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  easyTor
+//
+//  Created by Ivan Chistyakov on 5/27/18.
+//  Copyright © 2018 Ivan Chistyakov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ easyTor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  easyTor
+//
+//  Created by Ivan Chistyakov on 5/27/18.
+//  Copyright © 2018 Ivan Chistyakov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
