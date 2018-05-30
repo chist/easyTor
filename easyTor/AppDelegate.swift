0faa88d68ea113b47b8853f54af6464b28e7d75f
Ivan Chistyakov <ivanchistyakov@mba.local>
2018-05-30 20:38:55 +0300

    Menu item to enable / disable tor is now presented by custom view.

--- easyTor/AppDelegate.swift
+++ easyTor/AppDelegate.swift
@@ -12,19 +12,23 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    var switchViewController: SwitchViewController?
     var enableButton: NSMenuItem?
-    var state: Bool = false
     let torController = TorController()
     let iconSize = 16
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        // initialize switchViewController
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        self.switchViewController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "SwitchVC")) as? SwitchViewController
         
+        // create menu
         constructMenu()
         
+        // set menu bar icon
         let icon = NSImage(named: NSImage.Name("AppIcon"))
         icon?.isTemplate = true // best for dark mode
-        icon?.size = NSSize(width: self.iconSize, height: self.iconSize)
+        icon?.size = NSSize(width: iconSize, height: iconSize)
         statusItem.image = icon
     }
 
@@ -32,24 +36,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
-    @objc func setProxy(_ sender: Any?) {
-        if !self.state {
+    public func setProxy(toState state: NSControl.StateValue) {
+        if state == NSControl.StateValue.on {
             self.torController.enableProxy()
-            self.enableButton!.title = "Disable VPN"
-            self.state = true
         } else {
             self.torController.disableProxy()
-            self.enableButton!.title = "Enable VPN"
-            self.state = false
         }
     }
     
     func constructMenu() {
         let menu = NSMenu()
-        self.enableButton = NSMenuItem(title: "Enable VPN",
-                                       action: #selector(AppDelegate.setProxy(_:)),
-                                       keyEquivalent: "")
-        menu.addItem(self.enableButton!)
+        
+        // add menu item to turn on/off VPN
+        let switchButton = NSMenuItem()
+        switchButton.view = self.switchViewController?.view
+        menu.addItem(switchButton)
+        
         statusItem.menu = menu
     }
     
---
+++ easyTor/SwitchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SwitchViewController.swift
+//  easyTor
+//
+//  Created by Ivan Chistyakov on 5/30/18.
+//  Copyright © 2018 Ivan Chistyakov. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class SwitchViewController: ViewController {
+    let appDelegate = NSApplication.shared.delegate as! AppDelegate
+    @IBOutlet weak var enableVPNCheckBox: NSButton!
+    
+    @IBAction func enableVPNCheckBoxPressed(_ sender: Any) {
+        appDelegate.setProxy(toState: enableVPNCheckBox.state)
+    }
+}
