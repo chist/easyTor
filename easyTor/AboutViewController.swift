a28bf47250dffcf58355423d87da625490f92b52
Ivan Chistyakov <ivanchistyakov@mba.local>
2018-05-31 19:31:27 +0300

    Added hyperlinks to dependency projects.

--- easyTor/AboutViewController.swift
+++ easyTor/AboutViewController.swift
@@ -12,12 +12,21 @@ import Cocoa
 class AboutViewController: NSViewController {
     
     @IBOutlet weak var versionLabel: NSTextField!
+    @IBOutlet weak var linksLabel: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // display current app version and build number
         versionLabel.stringValue = self.getAppVersion()
+        
+        // make hyperlinks to dependency projects
+        self.linksLabel!.allowsEditingTextAttributes = true
+        let string = NSMutableAttributedString()
+        string.append(self.createHyperlink(url: "https://www.torproject.org", name: "Tor project"))
+        string.append(NSAttributedString(string: "\n"))
+        string.append(self.createHyperlink(url: "https://brew.sh", name: "Homebrew"))
+        self.linksLabel!.attributedStringValue = string
     }
     
     override func viewDidAppear() {
@@ -32,5 +41,15 @@ class AboutViewController: NSViewController {
         let dictionary = Bundle.main.infoDictionary!
         let version = dictionary["CFBundleShortVersionString"] as! String
         return "Version: \(version)"
+    }
+    
+    private func createHyperlink(url: String, name: String) -> NSAttributedString {
+        let linkTextAttributes: [NSAttributedStringKey: Any] = [
+            NSAttributedStringKey.underlineStyle: NSUnderlineStyle.styleSingle.rawValue,
+            NSAttributedStringKey.foregroundColor: NSColor.blue,
+            NSAttributedStringKey.link: url,
+            NSAttributedStringKey.font: NSFont.systemFont(ofSize: 14.0)
+        ]
+        return NSAttributedString(string: name, attributes: linkTextAttributes)
     }
 }
